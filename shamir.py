3e8c366d71cf7856aa7205db0476d98475d595cb
GEEG <e.glebov@innopolis.university>
2025-04-16 02:24:34 +0300

    Changed parameters of functions

--- shamir.py
+++ shamir.py
@@ -1,4 +1,4 @@
-def solve(input: int) -> str:
+def solve(input: int, total_keys: int, need_keys: int) -> str:
     # Convert the integer into shares using Shamir's scheme
     # Return string like "1:1234,2:5678,3:9101"
     return str(input*2)
